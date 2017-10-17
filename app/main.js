bc088ab49ab884335fed04119a1382fb25c4c536
caseydemo <cdemattina@gmail.com>
2017-10-17 11:35:40 -0400

    limpin through this...

--- app/main.js
+++ app/main.js
@@ -1,10 +1,47 @@
 Vue.component('space', {
+	template: "<div v-on:click='callSquare'>{{display}}</div>",
+	methods: {
+		callSquare: function(){
+			if(this.display===''){
+				// this is "I have not been clicked yet"
+				
+				this.display='X';
+				this.$emit('new');
+			}
+			else{
+				// "I have already been clicked"
+				
+				this.$emit('repeat');
+			}
+		}
+	},
+	props:['markers', 'turn'],
+	data: function(){
+		return{
+			display:''
+			
+		}
+		
+	}
 
 });
 
 var app = new Vue({
 
-  el: '#app'  
+  el: '#app',
+  data: {
+  	markers: ['X', 'O'],
+  	turn: 0
+
+  },
+  methods: {
+  	newClick: function(){
+  		alert('clicked a new space');
+  	}, 
+  	repeatClick: function(){
+  		alert('already clicked');
+  	}
+  }  
 
 
 
